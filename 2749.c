d460ce4e5801a5fc31aaca90abd1a9ff12fa9c5b
Sezan Mahmud <131911000+sezanmahmud30@users.noreply.github.com>
2025-02-17 18:13:39 +0600

    Add files via upload

---
+++ 2749.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("---------------------------------------\n");
+	printf("|x = 35                               |\n");
+	printf("|                                     |\n");
+	printf("|               x = 35                |\n");
+	printf("|                                     |\n");
+	printf("|                               x = 35|\n");
+	printf("---------------------------------------\n");
+
+    return 0;
+}
+
+
