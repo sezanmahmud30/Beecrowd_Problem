95a8cd7af2d8a0e670a4214803912ec921542e68
Sezan Mahmud <131911000+sezanmahmud30@users.noreply.github.com>
2025-02-19 22:50:04 +0600

    Add files via upload

---
+++ 2752.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"<AMO FAZER EXERCICIO NO URI>"<<endl;
+    cout<<"<    AMO FAZER EXERCICIO NO URI>"<<endl;
+    cout<<"<AMO FAZER EXERCICIO >"<<endl;
+    cout<<"<AMO FAZER EXERCICIO NO URI>"<<endl;
+    cout<<"<AMO FAZER EXERCICIO NO URI    >"<<endl;
+    cout<<"<AMO FAZER EXERCICIO NO URI>"<<endl;
+    cout<<"<          AMO FAZER EXERCICIO >"<<endl;
+    cout<<"<AMO FAZER EXERCICIO           >"<<endl;
+    return 0;
+}
