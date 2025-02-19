65d9ba1debec660b549983481b1b6d6a5866389c
Sezan Mahmud <131911000+sezanmahmud30@users.noreply.github.com>
2025-02-19 22:50:27 +0600

    Add files via upload

---
+++ 3302.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x,a;
+
+    cin >> x;
+
+     for(int i = 1; i <= x; i++){
+        //scanf("%d",&a);
+        cin >> a;
+        //printf("resposta %d: %d\n",i,x);
+        cout <<"resposta "<< i <<": "<< a << endl;
+    }
+
+
+    return 0;
+}
