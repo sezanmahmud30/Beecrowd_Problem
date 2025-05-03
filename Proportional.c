082906b4475dd3da23dae9b194efb3ff977e83f0
Sezan Mahmud <131911000+sezanmahmud30@users.noreply.github.com>
2025-05-03 21:02:36 +0600

    Add files via upload

---
+++ Proportional.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter three Number :");
+    scanf("%d %d %d",&a,&b,&c);
+
+    (b*b==a*c)?printf("Proportional"):printf("Not Proportional");
+
+    return 0;
+}
